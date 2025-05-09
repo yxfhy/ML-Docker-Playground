7eaa810d29cd3848c5bf4497aa5dc4f43e214ad4
yxfhy <yxfhy303@gmail.com>
2025-05-09 03:31:06 +0000

    Ctrl+Shift+Bでビルドが起動

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import os
 from dotenv import load_dotenv
 import argparse
 
+
 def main():
     # 引数のパース
-    parser = argparse.ArgumentParser(description="ML Docker Playground サンプルスクリプト")
-    parser.add_argument(
-        "--name",
-        type=str,
-        default="world",
-        help="挨拶する相手の名前"
+    parser = argparse.ArgumentParser(
+        description="ML Docker Playground サンプルスクリプト"
     )
+    parser.add_argument("--name", type=str, default="world", help="挨拶する相手の名前")
     args = parser.parse_args()
 
     # .env の読み込み
     load_dotenv()
+
     greeting = os.getenv("GREETING", "Hello")
 
     # 実行
     print(f"{greeting}, {args.name}!")
+
 
 if __name__ == "__main__":
     main()
